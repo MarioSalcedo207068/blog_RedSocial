31c0b2ebd82b0f96342ed6d23a4f73d587efe219
MarioSalcedo207068 <mario.salcedo207068@potros.itson.edu.mx>
2023-11-12 21:17:21 -0700

    update v.10

--- modeloBlog/src/main/java/Domain/Administrador.java
+++ modeloBlog/src/main/java/Domain/Administrador.java
@@ -4,10 +4,44 @@
  */
 package Domain;
 
+import java.util.Date;
+import java.util.List;
+
 /**
- *
- * @author aleja
+ * Clase que representa un administrador en el sistema. Hereda de la clase
+ * Usuario.
  */
-public class Administrador {
-    
+public class Administrador extends Usuario {
+
+    private List<Anclada> publicacionesAncladas;
+
+    public Administrador() {
+    }
+
+    public Administrador(String nombres, String apellidoPaterno, String apellidoMaterno, String telefono, String avatar, String ciudad, Date fechaNacimiento, Genero genero, Credencial credencial, Municipio municipio) {
+        super(nombres, apellidoPaterno, apellidoMaterno, telefono, avatar, ciudad, fechaNacimiento, genero, credencial, municipio);
+    }
+
+    public Administrador(int id, String nombres, String apellidoPaterno, String apellidoMaterno, String telefono, String avatar, String ciudad, Date fechaNacimiento, Genero genero, Credencial credencial, Municipio municipio) {
+        super(id, nombres, apellidoPaterno, apellidoMaterno, telefono, avatar, ciudad, fechaNacimiento, genero, credencial, municipio);
+    }
+
+    /**
+     * Obtiene la lista de publicaciones ancladas asociadas al administrador.
+     *
+     * @return La lista de publicaciones ancladas.
+     */
+    public List<Anclada> getPublicacionesAncladas() {
+        return publicacionesAncladas;
+    }
+
+    /**
+     * Establece la lista de publicaciones ancladas asociadas al administrador.
+     *
+     * @param publicacionesAncladas La lista de publicaciones ancladas.
+     */
+    public void setPublicacionesAncladas(List<Anclada> publicacionesAncladas) {
+        this.publicacionesAncladas = publicacionesAncladas;
+    }
+
 }
