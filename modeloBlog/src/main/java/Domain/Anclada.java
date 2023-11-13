a8524128374e8a57e8dd7e797f1b45381c06c1ca
MarioSalcedo207068 <mario.salcedo207068@potros.itson.edu.mx>
2023-11-12 21:17:55 -0700

    v0.11
    anclada

--- modeloBlog/src/main/java/Domain/Anclada.java
+++ modeloBlog/src/main/java/Domain/Anclada.java
@@ -4,10 +4,89 @@
  */
 package Domain;
 
+import java.util.Date;
+
 /**
- *
- * @author aleja
+ * Clase que representa una publicación anclada en el sistema. Hereda de la
+ * clase Publicacion.
  */
-class Anclada {
-    
+public class Anclada extends Publicacion {
+
+    private Administrador admin;
+
+    /**
+     * Constructor por defecto de la clase Anclada.
+     */
+    public Anclada() {
+    }
+
+    public Anclada(Administrador admin, Date fechaHoraCreacion, String titulo, String contenido) {
+        super(fechaHoraCreacion, titulo, contenido);
+        this.admin = admin;
+    }
+
+    /**
+     * Constructor que recibe un administrador para la publicación anclada.
+     *
+     * @param admin El administrador asociado a la publicación anclada.
+     */
+    public Anclada(Administrador admin) {
+        this.admin = admin;
+    }
+
+    /**
+     * Constructor que recibe un administrador, la fecha y hora de creación, el
+     * título, el contenido y la fecha y hora de edición de la publicación
+     * anclada.
+     *
+     * @param admin El administrador asociado a la publicación anclada.
+     * @param fechaHoraCreacion La fecha y hora de creación de la publicación
+     * anclada.
+     * @param titulo El título de la publicación anclada.
+     * @param contenido El contenido de la publicación anclada.
+     * @param fechaHoraEdicion La fecha y hora de edición de la publicación
+     * anclada.
+     */
+    public Anclada(Administrador admin, Date fechaHoraCreacion, String titulo, String contenido, Date fechaHoraEdicion) {
+        super(fechaHoraCreacion, titulo, contenido, fechaHoraEdicion);
+        this.admin = admin;
+    }
+
+    /**
+     * Constructor que recibe un administrador, el ID, la fecha y hora de
+     * creación, el título, el contenido y la fecha y hora de edición de la
+     * publicación anclada.
+     *
+     * @param admin El administrador asociado a la publicación anclada.
+     * @param id El ID de la publicación anclada.
+     * @param fechaHoraCreacion La fecha y hora de creación de la publicación
+     * anclada.
+     * @param titulo El título de la publicación anclada.
+     * @param contenido El contenido de la publicación anclada.
+     * @param fechaHoraEdicion La fecha y hora de edición de la publicación
+     * anclada.
+     */
+    public Anclada(Administrador administrador, int id, Date fechaHoraCreacion, String titulo, String contenido, Date fechaHoraEdicion) {
+        super(id, fechaHoraCreacion, titulo, contenido, fechaHoraEdicion);
+        this.admin = administrador;
+    }
+
+    /**
+     * Obtiene el administrador asociado a la publicación anclada.
+     *
+     * @return El administrador asociado a la publicación anclada.
+     */
+    public Administrador getAdmin() {
+        return admin;
+    }
+
+    /**
+     * Establece el administrador asociado a la publicación anclada.
+     *
+     * @param admin El administrador asociado a la publicación anclada.
+     */
+    public void setAdmin(Administrador admin) {
+        this.admin = admin;
+    }
+
 }
