e9b12b95bcb748fecc0fec57f1d5b593a86844f3
MarioSalcedo207068 <mario.salcedo207068@potros.itson.edu.mx>
2023-11-12 20:48:15 -0700

    Update v0.1

---
+++ modeloBlog/src/main/java/Domain/Municipio.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domain;
+
+/**
+ *
+ * @author aleja
+ */
+public class Municipio {
+    private int id;
+    private String nombre;
+}
---
+++ modeloBlog/src/main/java/Domain/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domain;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author aleja
+ */
+public class Usuario {
+    private String nombreCompleto;
+    private String correo;
+    private String contrasena;
+    private String avatar;
+    private String ciudad;
+    private Calendar fechaNacimiento;
+    private Genero genero;
+    
+}
