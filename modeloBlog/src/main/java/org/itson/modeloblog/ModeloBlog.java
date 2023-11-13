ce06c8230026194e0d4ca1b96811ea9d201e5287
MarioSalcedo207068 <mario.salcedo207068@potros.itson.edu.mx>
2023-11-12 20:41:04 -0700

    created class

---
+++ modeloBlog/src/main/java/org/itson/modeloblog/ModeloBlog.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.modeloblog;
+
+/**
+ *
+ * @author aleja
+ */
+public class ModeloBlog {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
