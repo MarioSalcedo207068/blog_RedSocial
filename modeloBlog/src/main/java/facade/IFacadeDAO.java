45efea36a8e84b9be17ae17a61570966de2da07f
Daniel Alameda <daoma222@gmail.com>
2023-11-14 00:23:59 -0700

    Se implementa la facade

---
+++ modeloBlog/src/main/java/facade/IFacadeDAO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+/**
+ *
+ * @author HP
+ */
+public interface IFacadeDAO {
+
+    public FacadeAdministradorDAO useAdministradorDAO();
+
+    public FacadeAncladaDAO useAncladaDAO();
+
+    public FacadeComentarioDAO useComentarioDAO();
+
+    public FacadeComunDAO useComunDAO();
+
+    public FacadeCredencialDAO useCredencialDAO();
+
+    public FacadeEstadoDAO useEstadoDAO();
+
+    public FacadeMunicipioDAO useMunicipioDAO();
+
+    public FacadeNormalDAO useNormalDAO();
+
+    public FacadePublicacionDAO usePublicacionDAO();
+
+    public FacadeUsuarioDAO useUsuarioDAO();
+}
